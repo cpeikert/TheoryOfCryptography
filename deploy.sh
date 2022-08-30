f4457fd495edbdad41b1e7cbd4fe19ab60d6e311
Chris Peikert <cpeikert@alum.mit.edu>
2022-08-30 09:58:01 -0400

    switched from Travis CI to GitHub Actions

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-git config --global user.email "travis@travis-ci.com"
-git config --global user.name "Travis CI"
-git checkout master
-git add -f *.pdf
-git commit -m "[skip ci] Automated PDF commit (build: $TRAVIS_BUILD_NUMBER)"
-git push --quiet "https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH"
